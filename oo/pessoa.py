14115260abf03b7f0759cb57b03306ed3a7132f6
David <maldonadopereira@gmail.com>
2021-11-25 19:42:03 -0300

    Criação atributo complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,18 @@
 class  Pessoa:
 
-    def __init__(self, nome = None, idade=35):
+    def __init__(self,*filhos,  nome = None, idade=None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    sophia = Pessoa(nome='Sophia')
+    david = Pessoa(sophia, nome = 'David', idade=29)
+    print(f'Nome: {david.nome}')
+    print(f'Idade: {david.idade}')
+    for filho in david.filhos:
+        print(f'Filhos: {filho.nome}')
+
