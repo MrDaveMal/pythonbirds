7be8e375ab1fc84602d087bd3b7fcd8f8d1a70e6
David <maldonadopereira@gmail.com>
2021-11-25 19:02:32 -0300

    Criação classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class  Pessoa:
-    pass
+
+    def __init__(self, nome = None, idade=35):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('Luciano')
+    print(Pessoa.cumprimentar(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
